3fca8a60574612a42e602b6216c6ed61f7cd127a
Reevak05 <kaveergera@gmail.com>
2021-11-15 22:21:48 -0800

    add search() and make insert() more efficient

--- src/SkipList.java
+++ src/SkipList.java
@@ -10,8 +10,9 @@ public class SkipList {
         maxHeight = 0; // All height values are zero-indexed, so they correspond with the nodes' array indexes
     }
 
-    public void insert(int data, int height) {
-        if (height > maxHeight) {
+    @Deprecated
+    public void oldInsert(int data, int height) {
+        if (height > maxHeight) { // If the new node's height is taller than head, increase the height of head (and change maxHeight accordingly)
             maxHeight = height;
             head.changeHeight(height);
         }
@@ -23,6 +24,59 @@ public class SkipList {
             }
             newNode.nextNodes[i] = currentNode.nextNodes[i]; // set the new node's pointer at that level to point to the next node
             currentNode.nextNodes[i] = newNode; // set the previous node's pointer at that level to point to the new node
+        }
+    }
+
+    public void insert(int data, int height) {
+        if (height > maxHeight) { // If the new node is taller than the head node, increase the height of head (and change maxHeight accordingly)
+            maxHeight = height;
+            head.changeHeight(height);
+        }
+
+        int currentLevel = maxHeight;
+        SkipListNode currentNode = head;
+        SkipListNode newNode = new SkipListNode(data, height); // the new node to be inserted
+        SkipListNode[] previousNodesForEachLevel = new SkipListNode[maxHeight + 1]; // the last visited nodes for each level; their pointers will be set to point to the new node
+
+        while (true) {
+            previousNodesForEachLevel[currentLevel] = currentNode; // Save the current node in its level's index in the list of last visited nodes
+
+            if (currentNode.nextNodes[currentLevel] == null || currentNode.nextNodes[currentLevel].data > data) { // If there is no node after the current node or if the node after the current node has a value greater than the new node, insert the node if on the lowest level or, if not, move down
+                if (currentLevel == 0) { // If on the lowest level, insert the new node
+                    for (int i = 0; i <= maxHeight; i++) { // Point the appropriate pointer of each the last visited nodes for each level to the new node
+                        SkipListNode nextNode = previousNodesForEachLevel[i].nextNodes[i];
+                        previousNodesForEachLevel[i].nextNodes[i] = newNode;
+                        newNode.nextNodes[i] = nextNode;
+                    }
+                    break; // Stop because the new node has been inserted
+                } else { // If not on the lowest level, go down a level
+                    currentLevel--;
+                }
+            } else { // If the value of the node to right is less than the new node's value, move to the right
+                currentNode = currentNode.nextNodes[currentLevel];
+            }
+        }
+    }
+
+    public boolean search(int data) {
+        int currentLevel = maxHeight;
+        SkipListNode currentNode = head;
+
+        while (true) {
+            if (currentNode.nextNodes[currentLevel] == null || currentNode.nextNodes[currentLevel].data > data) { // If there is no node after the current node or if the node after the current node has a value greater than the new node, insert the node if on the lowest level or, if not, move down
+                if (currentNode.data == data) {
+                    return true;
+                }
+                else {
+                    if (currentLevel == 0) {
+                        return false;
+                    } else { // If not on the lowest level, go down a level
+                        currentLevel--;
+                    }
+                }
+            } else { // If the value of the node to right is less than the new node's value, move to the right
+                currentNode = currentNode.nextNodes[currentLevel];
+            }
         }
     }
 
@@ -89,5 +143,12 @@ public class SkipList {
         skipList.delete(3);
         System.out.println("\n––––––\nExpected:\nhead -> * -> * -> *\nhead -> * -> * -> 12\nhead -> 2 -> 4 -> 12");
         skipList.print();
+        System.out.println("\n––––––\nExpected:\ntrue\nfalse\ntrue\nfalse\ntrue\nfalse\n\n");
+        System.out.println(skipList.search(12));
+        System.out.println(skipList.search(0));
+        System.out.println(skipList.search(4));
+        System.out.println(skipList.search(21));
+        System.out.println(skipList.search(2));
+        System.out.println(skipList.search(-300));
     }
 }
