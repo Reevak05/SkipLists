f3a50fda0a237cce7d15c73d56a082cb79c16c2b
Reevak05 <kaveergera@gmail.com>
2021-11-15 22:20:59 -0800

    add comments to instance variables, constructor, insert(), and print()

--- src/SkipList.java
+++ src/SkipList.java
@@ -1,11 +1,13 @@
 public class SkipList {
 
-    SkipListNode head;
-    int maxHeight;
+    // Instance variables
+    SkipListNode head; // the first node
+    int maxHeight; // the height of the tallest node
 
     SkipList() {
+        // Create head with the minimum value possible, so it always stays at the beginning of the list
         head = new SkipListNode(Integer.MIN_VALUE, 0);
-        maxHeight = 0;
+        maxHeight = 0; // All height values are zero-indexed, so they correspond with the nodes' array indexes
     }
 
     public void insert(int data, int height) {
@@ -14,20 +16,18 @@ public class SkipList {
             head.changeHeight(height);
         }
         SkipListNode newNode = new SkipListNode(data, height);
-        for (int i = 0; i <= newNode.height; i++) {
+        for (int i = 0; i <= newNode.height; i++) { // Start at the new node's height and decrease; for each level,
             SkipListNode currentNode = head;
-            while (currentNode.nextNodes[i] != null && currentNode.nextNodes[i].data < data) {
+            while (currentNode.nextNodes[i] != null && currentNode.nextNodes[i].data < data) { // find where the new node should go in order to maintain consecutive order
                 currentNode = currentNode.nextNodes[i];
             }
-            newNode.nextNodes[i] = currentNode.nextNodes[i];
-            currentNode.nextNodes[i] = newNode;
+            newNode.nextNodes[i] = currentNode.nextNodes[i]; // set the new node's pointer at that level to point to the next node
+            currentNode.nextNodes[i] = newNode; // set the previous node's pointer at that level to point to the new node
         }
     }
 
-    public boolean delete(int value){
-
+    public boolean delete(int value) {
         boolean wasPresent = false;
-
         for (int i = maxHeight; i >= 0; i--) {
             for (SkipListNode current = head; current.nextNodes[i] != null; current = current.nextNodes[i]) {
                 if (current.nextNodes[i].data == value) {
@@ -55,39 +55,39 @@ public class SkipList {
     }
 
     public void print() {
-        System.out.println(" \n\n Skip List: ");
-        for (int i = maxHeight; i >= 0; i--) {
+        System.out.println("\n\nSkip List: ");
+        for (int i = maxHeight; i >= 0; i--) { // Start at the highest level and decrease; for each level,
             SkipListNode currentNode = head;
-            System.out.print(currentNode.data);
-            while (currentNode.nextNodes[i] != null) {
+            System.out.print("head"); // print head
+            while (currentNode.nextNodes[i] != null) { // iterate through the following nodes and print the value of each one
                 currentNode = currentNode.nextNodes[i];
                 System.out.print(" -> " + ((currentNode != null) ? currentNode.data : "*"));
             }
-            System.out.println();
+            System.out.println(); // print a new line for the next level
         }
     }
 
     public static void main(String[] args) {
         SkipList skipList = new SkipList();
-        System.out.println("\n––––––\nExpected:\n-2147483648");
+        System.out.println("\n––––––\nExpected:\nhead");
         skipList.print();
         skipList.insert(4, 0);
-        System.out.println("\n––––––\nExpected:\n-2147483648 -> 4");
+        System.out.println("\n––––––\nExpected:\nhead -> 4");
         skipList.print();
         skipList.insert(2, 0);
-        System.out.println("\n––––––\nExpected:\n-2147483648 -> 2 -> 4");
+        System.out.println("\n––––––\nExpected:\nhead -> 2 -> 4");
         skipList.print();
         skipList.insert(12, 1);
-        System.out.println("\n––––––\nExpected:\n-2147483648 -> * -> * -> 12\n-2147483648 -> 2 -> 4 -> 12");
+        System.out.println("\n––––––\nExpected:\nhead -> * -> * -> 12\nhead -> 2 -> 4 -> 12");
         skipList.print();
         skipList.insert(9, 2);
-        System.out.println("\n––––––\nExpected:\n-2147483648 -> * -> * -> 9 -> *\n-2147483648 -> * -> * -> 9 -> 12\n-2147483648 -> 2 -> 4 -> 9 -> 12");
+        System.out.println("\n––––––\nExpected:\nhead -> * -> * -> 9 -> *\nhead -> * -> * -> 9 -> 12\nhead -> 2 -> 4 -> 9 -> 12");
         skipList.print();
         skipList.delete(9);
-        System.out.println("\n––––––\nExpected:\n-2147483648 -> * -> * -> * -> *\n-2147483648 -> * -> * -> * -> 12\n-2147483648 -> 2 -> 4 -> * -> 12");
+        System.out.println("\n––––––\nExpected:\nhead -> * -> * -> *\nhead -> * -> * -> 12\nhead -> 2 -> 4 -> 12");
         skipList.print();
         skipList.delete(3);
-        System.out.println("\n––––––\nExpected:\n-2147483648 -> * -> * -> * -> *\n-2147483648 -> * -> * -> * -> 12\n-2147483648 -> 2 -> 4 -> * -> 12");
+        System.out.println("\n––––––\nExpected:\nhead -> * -> * -> *\nhead -> * -> * -> 12\nhead -> 2 -> 4 -> 12");
         skipList.print();
     }
 }
